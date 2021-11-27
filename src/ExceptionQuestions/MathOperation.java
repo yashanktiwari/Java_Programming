bc81e488faf7c7147cb2185a076adbc096de6493
Yashank Tiwari <yashanktiwari@gmail.com>
2021-11-27 16:19:47 +0530

    Various Exceptions

--- src/ExceptionQuestions/MathOperation.java
+++ src/ExceptionQuestions/MathOperation.java
@@ -1,5 +1,10 @@
 package ExceptionQuestions;
+/*
+Write a class MathOperation which accepts 5 integers through command line. Create an array using these parameters.
+Loop through the array and obtain the sum and average of all the elements and display the result.
 
+Various exceptions that may arise like ArithmeticException, NumberFormatException, and so on should be handled.
+*/
 public class MathOperation {
     public static void main(String[] args) {
         int sum = 0;
