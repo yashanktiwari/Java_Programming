c051f869ce3b6e64312844e34f647dfa58ca22e1
Yashank Tiwari <yashanktiwari@gmail.com>
2021-12-06 13:16:23 +0530

    Question

--- src/com/myname/main/StudentMain.java
+++ src/com/myname/main/StudentMain.java
@@ -1,7 +1,18 @@
 package com.myname.main;
 
 /*
-
+A program to demonstrate custom exceptions.
+Create a com.myname.stuent.Student class with three private fields name, rollNo, & CPI.
+Create Getter and Setter for all the fields, override the public String toString() method of Object class,
+to represent Student objects as String. Create a com.myname.main.StudentMain class which is the implementation class
+contains main method and a search() method. main method will create a list of students i.e array and initialize
+each objects by taking input from user using java.util.Scanner class.
+This method will also call search method and print the result.
+search() method will accept two arguments first is Student[] list and int rollNo.
+This method will return Student object if rollNo matches with any of the Student object or else
+it will throw an user defined exception InvalidStudentException. com.myname.exception.
+InvalidStudentException is a class which extends java.lang.Exception class and have one parameterized constructor
+which accept an argument String message.
 */
 import com.myname.student.Student;
 import java.util.Scanner;
